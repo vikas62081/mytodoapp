4f3cb553f9144f74af6fc63a750e05ff9bcc2097
vikas62081 <vikas62081@gmail.com>
2020-09-04 14:35:04 +0530

    Updates

---
+++ precache-manifest.58e12ba8081a87a6708e9a0376ef8e98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04941500a0aa923ce61208c6fe0da72b",
+    "url": "/mytodoapp/index.html"
+  },
+  {
+    "revision": "8c7d55429a970dc2b525",
+    "url": "/mytodoapp/static/css/main.d0a7d6fa.chunk.css"
+  },
+  {
+    "revision": "ffda58a4ccc01f88e5e5",
+    "url": "/mytodoapp/static/js/2.7ab26d09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mytodoapp/static/js/2.7ab26d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7d55429a970dc2b525",
+    "url": "/mytodoapp/static/js/main.35853710.chunk.js"
+  },
+  {
+    "revision": "65b686abfceacd144e0e",
+    "url": "/mytodoapp/static/js/runtime-main.0da50b84.js"
+  }
+]);
